de710c415e5efbd93945a3f2e6dc7b41cc32fe15
Hiep Nguyen <ngochiep30051998@gmail.com>
2019-07-28 10:43:51 +0700

    deploy

--- constants/configs.js
+++ constants/configs.js
@@ -1,9 +1,18 @@
+// const DB_CONFIG = {
+//     host: "localhost",
+//     port: "3306",
+//     database: "db_projectmanager",
+//     user: "root",
+//     password: "",
+//     prefix: ""
+// }
+
 const DB_CONFIG = {
-    host: "localhost",
+    host: "us-cdbr-iron-east-02.cleardb.net",
     port: "3306",
-    database: "db_projectmanager",
-    user: "root",
-    password: "",
+    database: "heroku_d91c22223a25724",
+    user: "b27b75b7e46d18",
+    password: "2bddc4a4",
     prefix: ""
 }
 
